db23fbc38a09060b0a0e48a1034ccd3c5ea626d5
Federico Secco <federico.secco7@gmail.com>
2024-07-02 23:13:28 +0200

    ADD transloco
    Refs https://github.com/roomie-club/roomie-frontend/issues/1

--- apps/roomie-client/src/app/app.config.ts
+++ apps/roomie-client/src/app/app.config.ts
@@ -1,10 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco } from '@jsverse/transloco';
+import { TranslocoHttpLoader } from './core/transloco-loader';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
-  ],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(appRoutes),
+        provideHttpClient(),
+        provideTransloco({
+            config: {
+                availableLangs: ['en', 'it', 'ro'],
+                defaultLang: 'en',
+                reRenderOnLangChange: true,
+                prodMode: !isDevMode(),
+                missingHandler: { allowEmpty: false, logMissingKey: true },
+            },
+            loader: TranslocoHttpLoader,
+        }),
+    ],
 };
